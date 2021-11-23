abc39ec613cd387044b668a6947f26e854bfec08
Khoi Hoang <khoii87@gmail.com>
2021-11-23 23:04:39 +0100

    Add: ✨ Simple example how the react-hook-form is working for form validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,35 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  )
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import './App.css';
+interface interfaceFormInput {
+	email: string;
+	password: string;
 }
 
-export default App
+const App: React.FC = () => {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<interfaceFormInput>();
+
+  const formSubmitHandler: SubmitHandler<interfaceFormInput> = (data: interfaceFormInput) => {
+    console.log('form data is', data);
+  }
+
+  console.log('Watch variable email', watch('email'));
+
+	return (
+		<main>
+			<form onSubmit={handleSubmit(formSubmitHandler)} className='form'>
+				<input type='text' defaultValue='example@text.com' {...register('email')} />
+				<input type='text' {...register('password', { required: true })} />
+				{errors.password && <span>This password field is required!</span>}
+				<input type='submit' value='Submit' />
+			</form>
+		</main>
+	);
+};
+
+export default App;
