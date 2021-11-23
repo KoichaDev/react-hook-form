54ddfd220355b8090c8bcab710f92d1275ece0b5
Khoi Hoang <khoii87@gmail.com>
2021-11-23 23:10:24 +0100

    Add: ✨ Yup valication schema

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import './App.css';
 interface interfaceFormInput {
 	email: string;
 	password: string;
 }
+
+const schema = yup.object().shape({
+	email: yup.string().email().required(),
+	password: yup.string().min(4).max(20).required(),
+});
 
 const App: React.FC = () => {
 	const {
@@ -12,20 +19,22 @@ const App: React.FC = () => {
 		handleSubmit,
 		watch,
 		formState: { errors },
-	} = useForm<interfaceFormInput>();
+	} = useForm<interfaceFormInput>({
+		resolver: yupResolver(schema),
+	});
 
-  const formSubmitHandler: SubmitHandler<interfaceFormInput> = (data: interfaceFormInput) => {
-    console.log('form data is', data);
-  }
+	const formSubmitHandler: SubmitHandler<interfaceFormInput> = (data: interfaceFormInput) => {
+		console.log('form data is', data);
+	};
 
-  console.log('Watch variable email', watch('email'));
+	console.log('Watch variable email', watch('email'));
 
 	return (
 		<main>
 			<form onSubmit={handleSubmit(formSubmitHandler)} className='form'>
 				<input type='text' defaultValue='example@text.com' {...register('email')} />
-				<input type='text' {...register('password', { required: true })} />
-				{errors.password && <span>This password field is required!</span>}
+				<input type='text' {...register('password')} />
+				{errors.password && errors.password?.message && <span>{errors.password.message}</span>}
 				<input type='submit' value='Submit' />
 			</form>
 		</main>
